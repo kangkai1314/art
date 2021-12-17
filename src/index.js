bd46eac60652a1c1ec5251764a94518e13037939
kangkai <1253393765@qq.com>
2021-12-17 18:27:01 +0800

    201212 upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('tttttt')
+import './styles/index.css'
+import './styles/index.less'
+import App from './App'
+import Vue from 'vue'
+new Vue({
+    render:h=>h(App)
+}).$mount('#app')
+
